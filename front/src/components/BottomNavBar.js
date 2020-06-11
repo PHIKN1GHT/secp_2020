2f2894df2e5a7bd37486c057371c4322c07b123e
CR <1361439473@qq.com>
2020-06-11 16:26:14 +0800

    desomebug

--- front/src/components/BottomNavBar.js
+++ front/src/components/BottomNavBar.js
@@ -9,7 +9,7 @@ export default function BottomNavBar(props) {
                 {
                     props['props'].map(val =>
                         <div className='item'>
-                            <Link to={val['url']}>
+                            <Link className='link' to={val['url']}>
                                 <div className='icon'>
                                     <div className='img'>{val['icon']}</ div>
                                     <div className='text'>{val['text']}</div>
