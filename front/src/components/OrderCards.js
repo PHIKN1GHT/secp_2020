138eed1477288536695f865c483293852bf1e885
CR <1361439473@qq.com>
2020-06-12 16:38:01 +0800

    add react-window. before yarn start you should yarn.

--- front/src/components/OrderCards.js
+++ front/src/components/OrderCards.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { ListItemText, ListItem } from '@material-ui/core';
+import { FixedSizeList } from 'react-window';
 
+
+function renderRow(props) {
+    const { index, style } = props;
+
+    return (
+        <ListItem button style={style} key={index}>
+            <ListItemText primary={`Item ${index + 1}`} />
+        </ListItem>
+    );
+}
 export default function OrderCards(props) {
 
     return (<>
         <div className='order-cards'>
-            <div></div>
+            <FixedSizeList height={400} width={300} itemSize={46} itemCount={200}>
+                {renderRow}
+            </FixedSizeList><div></div>
         </div>
     </>)
 }
