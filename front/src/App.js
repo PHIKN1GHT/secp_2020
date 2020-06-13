2a8c4b77fe143fa301fade65c07f2f4b3a6a448f
CR <1361439473@qq.com>
2020-06-13 16:15:30 +0800

    del some useless thing

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Switch } from 'react-router-dom';
 import './css/Login.css';
-import './css/PicTxtCard.css';
 import './css/UserInfo.css';
 import './css/BottomNavBar.css';
 import './css/Orders.css';
