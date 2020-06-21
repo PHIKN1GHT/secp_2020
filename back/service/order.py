372036a2a82135b681c22639483323ab95ac7eb2
PHIN1GHT <kale1d0@qq.com>
2020-06-21 16:05:55 +0800

    fix order state

--- back/service/order.py
+++ back/service/order.py
@@ -51,17 +51,17 @@ def allOrder():
                         'count': order.count,
                     })
                 if virorder.cancelled:
-                    status="已取消"
-                elif virorder.archived:
-                    status="已收货"
+                    status="已撤销"
+                #elif virorder.archived:
+                #    status=
                 elif virorder.delivered:
-                    status="已发货"
+                    status="已收货"
                 elif virorder.accepted:
-                    status="已接单"
+                    status="待收货"
                 elif virorder.paid:
-                    status="已支付"
+                    status="待发货"
                 else:
-                    status="未支付"
+                    status="已创建"
                 # addr=sess.query(Address).filter_by(id=virorder.address_id).first()
                 # orders.append((virorder.id, virorder.createTime, suborders, addr.receiver, addr.phonenumber, addr.address, status))
 
